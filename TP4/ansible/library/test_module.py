829131e68ff926122f727d1b35781242c8fec14c
Paulinefd <faddapau@gmail.com>
2022-07-21 08:52:39 +0200

    maj

--- TP4/ansible/library/test_module.py
+++ TP4/ansible/library/test_module.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import AnsibleModule
+import os
+import zipfile
 
-import shutil
+def zipdir(/files/TP4, ziph)
+    for root, dirs, files in os.walk(/files/TP4):
+            for file in files:
+                ziph.write(os.path.join(root,file))
+zipf= zipfile.Zipfile('Zipped_file.zip','w', zipfile.ZIP_DEFLATED)
+zipdir('backup', zipf)
+zipf.close()
 
-shutil.copytree ("/usr, ~/archives")
+# def main():
+#     module = AnsibleModule(
+
+#     )
+# if __name__ == '__main__':
+#     main()
+
